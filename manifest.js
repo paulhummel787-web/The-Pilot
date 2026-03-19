73ba50a3c98729a378c938cda005b04341e2a2b0
paulhummel787-web <paulhummel787@gmail.com>
2026-03-19 15:33:39 -0400

    Update and rename engine.js to manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "Apex Hive OS",
+  "short_name": "ApexHive",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#050502",
+  "theme_color": "#f59e0b",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/919/919825.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
